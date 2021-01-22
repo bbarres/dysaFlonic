a2c3c057498b0817b750b1ce48058e2036e30131
bbarres <benoit.barres@gmail.com>
2021-01-22 01:42:53 +0100

    correction of the model

--- compFilm.R
+++ compFilm.R
@@ -22,18 +22,20 @@ temp.m1<-drm(nb_mtot/(nb_mtot+nb_vi)~dose,
              weights=(nb_mtot+nb_vi),
              data=tempdat,
              curveid=ana_id,
-             fct=LN.3u())
+             fct=LN.3u(),type="binomial")
 plot(temp.m1,ylim=c(0,1.1),xlim=c(0,100),
      main=names(table(dataSeal$ech_id))[1])
+compParm(temp.m1,"e")
 
 tempdat<-dataSeal[dataSeal$ech_id==names(table(dataSeal$ech_id))[2],]
 temp.m2<-drm(nb_mtot/(nb_mtot+nb_vi)~dose,
              weights=(nb_mtot+nb_vi),
              data=tempdat,
              curveid=ana_id,
-             fct=LN.3u())
+             fct=LN.3u(),type="binomial")
 plot(temp.m2,ylim=c(0,1.1),xlim=c(0,100),
      main=names(table(dataSeal$ech_id))[2])
+compParm(temp.m2,"e")
 
 
 ##############################################################################/
@@ -45,18 +47,20 @@ temp<-drm(nb_mtot/(nb_mtot+nb_vi)~dose,
              weights=(nb_mtot+nb_vi),
              data=tempdat,
              curveid=ana_id,
-             fct=LN.3u())
+             fct=LN.3u(),type="binomial")
 plot(temp,ylim=c(0,1.1),xlim=c(0,100),
      main="clear seal")
+compParm(temp,"e")
 
 tempdat<-dataSeal[dataSeal$ana_id==names(table(dataSeal$ana_id))[c(2,4)],]
 temp<-drm(nb_mtot/(nb_mtot+nb_vi)~dose,
           weights=(nb_mtot+nb_vi),
           data=tempdat,
           curveid=ana_id,
-          fct=LN.3u())
+          fct=LN.3u(),type="binomial")
 plot(temp,ylim=c(0,1.1),xlim=c(0,100),
      main="porous seal")
+compParm(temp,"e")
 
 
 
